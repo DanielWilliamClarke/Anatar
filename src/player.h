80f73b4850f6cc92f964ed041cbae248daf9ec67
Daniel William Clarke <clarkit@gmail.com>
2020-06-10 20:30:23 +0100

    made inputless update in player private

--- src/player.h
+++ src/player.h
@@ -9,12 +9,11 @@ public:
 	  std::shared_ptr<AnimationComponent> animationComponent,
 	  std::shared_ptr<MovementComponent> movementComponent);
   virtual ~Player() = default;
-
-  virtual void Update(float dt) const {};
   virtual void Update(Input in, float dt) const;
   virtual void Draw(sf::RenderTarget &target, float interp) const;
 
 private: 
+	virtual void Update(float dt) const {};
 	void InitPlayer() const;
 	void SetAnimations() const;
 
